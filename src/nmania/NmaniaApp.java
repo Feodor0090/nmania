8b87aea5630ae7ef3903a5349ff2bc3afddfb11f
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-01-29 22:02:47 +0300

    Create project

---
+++ src/nmania/NmaniaApp.java
@@ -0,0 +1,27 @@
+package nmania;
+
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+public final class NmaniaApp extends MIDlet {
+
+	public NmaniaApp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void pauseApp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void startApp() throws MIDletStateChangeException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
