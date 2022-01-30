cb4a25723a7075d9f8517d56cc8501f850b5ce89
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-01-30 06:13:48 +0300

    Add random notes in test run

--- src/nmania/NmaniaApp.java
+++ src/nmania/NmaniaApp.java
@@ -1,11 +1,14 @@
 package nmania;
 
+import java.util.Random;
+
 import javax.microedition.lcdui.Display;
 import javax.microedition.midlet.MIDlet;
 import javax.microedition.midlet.MIDletStateChangeException;
 
 import org.json.me.JSONObject;
 
+import nmania.Beatmap.ManiaNote;
 import symnovel.SNUtils;
 
 public final class NmaniaApp extends MIDlet {
@@ -30,6 +33,7 @@ public final class NmaniaApp extends MIDlet {
 			s.folderName="test";
 			Beatmap b = new Beatmap(new JSONObject(SNUtils.readJARRes("/test/map.json", 4096)));
 			b.set = s;
+			b.notes = RandomNotes(1000, 4, 60000f/160);
 			Player p = new Player(b);
 			Display.getDisplay(inst).setCurrent(p);
 			Thread t = new PlayerThread(p);
@@ -37,6 +41,15 @@ public final class NmaniaApp extends MIDlet {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	ManiaNote[] RandomNotes(int c, int cols, float beatLength) {
+		Random r = new Random();
+		ManiaNote[] n = new ManiaNote[c];
+		for (int i = 0; i < c; i++) {
+			n[i] = new ManiaNote((int) (i*beatLength), r.nextInt(cols)+1, (int)(r.nextInt()%2==1?beatLength/2:0));
+		}
+		return n;
 	}
 
 }
