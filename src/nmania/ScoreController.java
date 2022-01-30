170ba879d74b6c3ad1c735c69f3852d10bb97abf
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-01-30 03:11:10 +0300

    Create ScoreController.java

---
+++ src/nmania/ScoreController.java
@@ -0,0 +1,26 @@
+package nmania;
+
+final class ScoreController {
+	public final int[] hits = new int[6];
+	public int maxHitScore;
+	public int currentHitScore;
+	public int maxCombo;
+	public int currentCombo;
+	
+	private static final int[] scores = new int[] {0,50,100,200,300,300};
+	
+	public final void CountHit(int type) {
+		hits[type]++;
+		if(type==0) {
+			if(currentCombo>maxCombo) maxCombo = currentCombo;
+			currentCombo = 0;
+		} else
+			currentCombo++;
+		maxHitScore+=scores[5];
+		currentHitScore+=scores[type];
+	}
+	
+	public final int GetAccuracy() {
+		return currentHitScore*10000/maxHitScore;
+	}
+}
