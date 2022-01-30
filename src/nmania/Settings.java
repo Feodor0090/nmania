6889946981118ae9932c448f46116768e8f536cd
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-01-31 00:37:24 +0300

    Update Settings.java

--- src/nmania/Settings.java
+++ src/nmania/Settings.java
@@ -5,16 +5,16 @@ public final class Settings {
 	public static int[][] keyLayout = new int[][] {
 			new int[] { 1},
 			new int[] { 1,2},
-			new int[] { 1,2,3},
-			new int[] { 'q','w','o','p'},
+			new int[] { '1','2','3'},
+			new int[] { 'q','e','i','p'},
 			new int[] { 1,2,3,4,5},
 			new int[] { 1,2,3,4,5,6},
 			new int[] { 1,2,3,4,5,6,7},
 	};
 	public static int pauseKey;
-	public static int leftOffset = 75;
-	public static int columnWidth = 50;
-	public static int holdWidth = 30;
-	public static int keyboardHeight = 100;
+	public static int leftOffset = 30;
+	public static int columnWidth = 30;
+	public static int holdWidth = 20;
+	public static int keyboardHeight = 50;
 	public static int noteHeight = 20;
 }
