d6d1f2912bf809a6a1f29a8b60a93c9f8df4c4d9
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-02-01 14:52:39 +0300

    Save layout on screen leave

--- src/nmania/ui/KeyboardLayoutSelect.java
+++ src/nmania/ui/KeyboardLayoutSelect.java
@@ -45,6 +45,7 @@ public class KeyboardLayoutSelect extends Canvas {
 				selected = items.length - 1;
 		} else if (k == -5 || k == -6 || k == 32 || k == '5') {
 			if (selected == 0) {
+				Settings.Save();
 				Nmania.Push(prev);
 				return;
 			} else {
