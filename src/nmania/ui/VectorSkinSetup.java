333cdd7f63ff95189e0de3581938a5662f30aaf3
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-02-03 01:03:31 +0300

    Add way to edit color palletes

--- src/nmania/ui/VectorSkinSetup.java
+++ src/nmania/ui/VectorSkinSetup.java
@@ -1,15 +1,20 @@
 package nmania.ui;
 
+import javax.microedition.lcdui.Choice;
+import javax.microedition.lcdui.ChoiceGroup;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Displayable;
 import javax.microedition.lcdui.Form;
 import javax.microedition.lcdui.Gauge;
+import javax.microedition.lcdui.Item;
+import javax.microedition.lcdui.ItemCommandListener;
+import javax.microedition.lcdui.StringItem;
 
 import nmania.Nmania;
 import nmania.Skin;
 
-public class VectorSkinSetup extends Form implements CommandListener {
+public class VectorSkinSetup extends Form implements CommandListener, ItemCommandListener {
 
 	public VectorSkinSetup(SkinSelect ss) {
 		super("Vector skin setup");
@@ -21,18 +26,30 @@ public class VectorSkinSetup extends Form implements CommandListener {
 		append(leftO);
 		append(holdW);
 		append(noteH);
+		noteColors.setItemCommandListener(this);
+		noteColors.setDefaultCommand(edit);
+		append(noteColors);
+		holdColors.setItemCommandListener(this);
+		holdColors.setDefaultCommand(edit);
+		append(holdColors);
+		append(noteFillOptions);
 		append("No more settings can be changed for now. Check this menu later.");
 	}
 
 	private final Command back = new Command("Back", Command.BACK, 1);
+	private final Command edit = new Command("Edit", Command.ITEM, 1);
 	final SkinSelect prev;
 	final Skin skin = Nmania.skin;
-	
+
 	Gauge colW = new Gauge("Column width", true, 150, skin.columnWidth);
 	Gauge keysH = new Gauge("Keyboard height", true, 200, skin.keyboardHeight);
 	Gauge leftO = new Gauge("Left offset", true, 800, skin.leftOffset);
 	Gauge holdW = new Gauge("Hold trail width", true, 150, skin.holdWidth);
 	Gauge noteH = new Gauge("Note height", true, 100, skin.noteHeight);
+	StringItem noteColors = new StringItem(null, "Notes pallete", StringItem.BUTTON);
+	StringItem holdColors = new StringItem(null, "Holds pallete", StringItem.BUTTON);
+	ChoiceGroup noteFillOptions = new ChoiceGroup("Note fill options", Choice.MULTIPLE,
+			new String[] { "Use vertical gradient", "Use different palletes for notes and hold heads" }, null);
 
 	void Apply() {
 		skin.columnWidth = colW.getValue();
@@ -41,12 +58,27 @@ public class VectorSkinSetup extends Form implements CommandListener {
 		skin.holdWidth = holdW.getValue();
 		skin.noteHeight = noteH.getValue();
 	}
+
 	public void commandAction(Command c, Displayable arg1) {
 		if (c == back) {
 			Apply();
 			skin.Save();
 			Nmania.Push(prev);
 			return;
+		}
+	}
+
+	public void commandAction(Command c, Item i) {
+		if (c == edit) {
+			int[] pallete;
+			if (i == noteColors) {
+				pallete = skin.noteColors;
+			} else if (i == holdColors) {
+				pallete = skin.holdColors;
+			} else {
+				return;
+			}
+			Nmania.Push(new ColorPalleteEditor(pallete, this));
 		}
 	}
 }
