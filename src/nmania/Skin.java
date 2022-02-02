ef85cf63d48033d4251e0bbd30268240966749ac
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-02-02 07:09:09 +0300

    Create Skin.java

---
+++ src/nmania/Skin.java
@@ -0,0 +1,69 @@
+package nmania;
+
+import javax.microedition.rms.RecordStore;
+
+import org.json.me.JSONObject;
+
+import symnovel.SNUtils;
+
+public class Skin {
+
+	public Skin() {
+		try {
+			RecordStore r = RecordStore.openRecordStore("nmania_skin", true);
+
+			if (r.getNumRecords() < 1) {
+				r.closeRecordStore();
+				throw new RuntimeException("No saved settings");
+			}
+			byte[] d = r.getRecord(1);
+			r.closeRecordStore();
+
+			// parse
+			JSONObject j = new JSONObject(new String(d));
+
+			rich = j.getBoolean("rich");
+			keyboardHeight = j.getInt("keyboardheight");
+			leftOffset = j.getInt("leftoffset");
+			columnWidth = j.getInt("columnwidth");
+			holdWidth = j.getInt("holdwidth");
+			noteHeight = j.getInt("noteheight");
+
+			noteColors = SNUtils.json2intArray(j.getJSONArray("notecolors"));
+			holdColors = SNUtils.json2intArray(j.getJSONArray("holdcolors"));
+			keyColors = SNUtils.json2intArray(j.getJSONArray("keycolors"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public boolean rich = false;
+	public int keyboardHeight = 50;
+	public int leftOffset = 30;
+	public int columnWidth = 30;
+	public int holdWidth = 20;
+	public int noteHeight = 20;
+	public int[] noteColors;
+	public int[] holdColors;
+	public int[] keyColors;
+
+	public int GetColumnWidth() {
+		return columnWidth;
+	}
+
+	public int GetNoteHeight() {
+		return noteHeight;
+	}
+
+	public int GetKeyboardHeight() {
+		return keyboardHeight;
+	}
+
+	public void Load() {
+
+	}
+
+	public void Save() {
+
+	}
+}
