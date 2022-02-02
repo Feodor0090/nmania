21539636c60d4f5c0d6c1302768c1ba2f9204c97
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-02-02 07:54:16 +0300

    Implement skin saving

--- src/nmania/Skin.java
+++ src/nmania/Skin.java
@@ -1,7 +1,10 @@
 package nmania;
 
+import java.util.Vector;
+
 import javax.microedition.rms.RecordStore;
 
+import org.json.me.JSONArray;
 import org.json.me.JSONObject;
 
 import symnovel.SNUtils;
@@ -66,6 +69,32 @@ public class Skin {
 	}
 
 	public void Save() {
+		try {
+			JSONObject j = new JSONObject();
 
+			j.accumulate("rich", new Boolean(rich));
+			j.accumulate("keyboardheight", new Integer(keyboardHeight));
+			j.accumulate("leftoffset", new Integer(leftOffset));
+			j.accumulate("columnwidth", new Integer(columnWidth));
+			j.accumulate("holdwidth", new Integer(holdWidth));
+			j.accumulate("noteheight", new Integer(noteHeight));
+
+			{Vector v = new Vector();for(int i=0;i<noteColors.length;i++) {v.addElement(new Integer(noteColors[i]));}j.accumulate("notecolors", new JSONArray(v));}
+			{Vector v = new Vector();for(int i=0;i<holdColors.length;i++) {v.addElement(new Integer(holdColors[i]));}j.accumulate("holdcolors", new JSONArray(v));}
+			{Vector v = new Vector();for(int i=0;i<keyColors.length;i++) {v.addElement(new Integer(keyColors[i]));}j.accumulate("keycolors", new JSONArray(v));}
+			{Vector v = new Vector();for(int i=0;i<holdKeyColors.length;i++) {v.addElement(new Integer(holdKeyColors[i]));}j.accumulate("holdkeycolors", new JSONArray(v));}
+			
+			// writing
+			byte[] d = j.toString().getBytes();
+			RecordStore r = RecordStore.openRecordStore("nmania_skin", true);
+
+			if (r.getNumRecords() == 0) {
+				r.addRecord(new byte[1], 0, 1);
+			}
+			r.setRecord(1, d, 0, d.length);
+			r.closeRecordStore();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
