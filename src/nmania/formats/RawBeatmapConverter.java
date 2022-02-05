6bade7def833be9fcbdd58568883921edbb44551
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-02-05 07:13:11 +0300

    Bypass UTF-8-BOM

--- src/nmania/formats/RawBeatmapConverter.java
+++ src/nmania/formats/RawBeatmapConverter.java
@@ -3,10 +3,10 @@ package nmania.formats;
 public class RawBeatmapConverter {
 	public static IRawBeatmap FromText(String raw) throws InvalidBeatmapTypeException {
 		IRawBeatmap rb;
-		if (raw.startsWith("osu file format")) {
-			rb = new RawOsuBeatmap(raw);
-		} else if (raw.charAt(0) == '{') {
+		if (raw.charAt(0) == '{') {
 			rb = new RawNmaniaBeatmap(raw);
+		} else if (raw.indexOf("osu file format") < 33) {
+			rb = new RawOsuBeatmap(raw);
 		} else
 			throw new InvalidBeatmapTypeException("This is not osu! nor nmania beatmap. Is the file damaged?");
 		return rb;
