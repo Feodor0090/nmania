f1579192e08d687bf0d40105253e532371627b19
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-02-08 06:14:41 +0300

    Remove old code

--- src/nmania/Beatmap.java
+++ src/nmania/Beatmap.java
@@ -1,10 +1,8 @@
 package nmania;
 
 import org.json.me.JSONArray;
-import org.json.me.JSONObject;
 
 import symnovel.IComparable;
-import symnovel.SNUtils;
 
 public final class Beatmap {
 	public int id;
@@ -19,30 +17,6 @@ public final class Beatmap {
 	public int defaultSampleSet = 0;
 
 	public Beatmap() {
-	}
-
-
-	public Beatmap(JSONObject j) {
-		id = j.getInt("id");
-		diffName = j.getString("name");
-		image = j.getString("image");
-		audio = j.getString("audio");
-		columnsCount = j.getInt("columns");
-		difficulty = Float.parseFloat(j.getString("difficulty"));
-
-		final JSONArray p = j.getJSONArray("timings");
-		points = new TimingPoint[p.length()];
-		for (int i = 0; i < p.length(); i++) {
-			String[] s = SNUtils.split(p.getString(i), ';', 4);
-			float beatLen = s[2].equals("bpm") ? (60000f / Float.parseFloat(s[3])) : Float.parseFloat(s[3]);
-			points[i] = new TimingPoint(Integer.parseInt(s[0]), Integer.parseInt(s[1]), beatLen);
-		}
-
-		final JSONArray n = j.getJSONArray("notes");
-		notes = new ManiaNote[n.length()];
-		for (int i = 0; i < n.length(); i++) {
-			notes[i] = new ManiaNote(n.getJSONArray(i));
-		}
 	}
 
 	public String ToGlobalPath(String local) {
