82cbcac3cb1f86e0bfdbf84c6dd420cda5043e02
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-10-24 03:57:49 +0300

    Add global logger

---
+++ src/nmania/GL.java
@@ -0,0 +1,46 @@
+package nmania;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import javax.microedition.io.Connector;
+import javax.microedition.io.file.FileConnection;
+
+public class GL {
+
+	private static FileConnection fc;// ?dbg
+	private static OutputStream stream;// ?dbg
+
+	public static void Create() {// ?dbg
+		if (stream != null)// ?dbg
+			return;// ?dbg
+		try {// ?dbg
+			fc = (FileConnection) Connector// ?dbg
+					.open("file:///" + Settings.workingFolder + "log-" + System.currentTimeMillis() + ".log");// ?dbg
+			if (!fc.exists())// ?dbg
+				fc.create();// ?dbg
+			else// ?dbg
+				fc.truncate(0);// ?dbg
+			stream = fc.openOutputStream();// ?dbg
+			Log("Starting log for nmania session...");// ?dbg
+			Log("Running " + Nmania.version + " on " + System.getProperty("microedition.platform"));// ?dbg
+		} catch (Exception e) {// ?dbg
+		} // ?dbg
+	}// ?dbg
+
+	public static void Log(String s) { // ?dbg
+		try {// ?dbg
+			System.out.println(s); // ?dbg
+			stream.write(s.getBytes());// ?dbg
+			stream.write('\n');// ?dbg
+			stream.flush();// ?dbg
+		} catch (IOException e) {// ?dbg
+		} // ?dbg
+	}// ?dbg
+
+	public static void LogStats() {// ?dbg
+		long total = Runtime.getRuntime().totalMemory() / 1024;// ?dbg
+		long free = Runtime.getRuntime().freeMemory() / 1024;// ?dbg
+		Log("Memory: T=" + total + " F=" + free + " U=" + (total - free) + "; Threads: " + Thread.activeCount());// ?dbg
+	}// ?dbg
+}
