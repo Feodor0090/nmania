eefa15208f8d840f320f5235d3770fd59fa1fee1
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-10-25 19:29:03 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -50,30 +50,29 @@ WORK_DIR=`dirname $0`
 cd ${WORK_DIR}
 
 echo "Creating or cleaning directories..."
-mkdir -p ../tmpclasses
-mkdir -p ../classes
-rm -rf ../tmpclasses/*
-rm -rf ../classes/*
+mkdir -p ./tmpclasses
+mkdir -p ./classes
+rm -rf ./tmpclasses/*
+rm -rf ./classes/*
 
 echo "Compiling source files..."
 ${JAVAC} \
     -bootclasspath ${CLDCAPI}${PATHSEP}${MIDPAPI} \
     -source 1.3 \
     -target 1.3 \
-    -d ../tmpclasses \
-    -classpath ../tmpclasses${PATHSEP}${CLASSPATH} \
-	-extdirs ../lib \
+    -d ./tmpclasses \
+    -classpath ./tmpclasses${PATHSEP}${CLASSPATH} \
     `find ./src -name '*'.java`
 
 echo "Preverifying class files..."
 
 ${PREVERIFY} \
-    -classpath ${CLDCAPI}${PATHSEP}${MIDPAPI}${PATHSEP}${CLASSPATH}${PATHSEP}../tmpclasses \
-    -d ../classes \
-    ../tmpclasses
+    -classpath ${CLDCAPI}${PATHSEP}${MIDPAPI}${PATHSEP}${CLASSPATH}${PATHSEP}./tmpclasses \
+    -d ./classes \
+    ./tmpclasses
 
 echo "Jaring preverified class files..."
-${JAR} cmf ${MANIFEST} ${APP}.jar -C ../classes .
+${JAR} cmf ${MANIFEST} ${APP}.jar -C ./classes .
 
 if [ -d ${RES} ] ; then
   ${JAR} uf ${APP}.jar -C ${RES} .
