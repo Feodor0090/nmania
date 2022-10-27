a12b6b5f99f5f7e94906a40e7710f559c637ea34
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-10-28 00:48:44 +0300

    Fix formatting

--- src/nmania/replays/ReplayReaderStream.java
+++ src/nmania/replays/ReplayReaderStream.java
@@ -11,47 +11,50 @@ import symnovel.SNUtils;
  *
  */
 public final class ReplayReaderStream extends OutputStream {
-		int lastKeys = 0;
-		int lastTime = 0;
-		int nextFrame = 0;
-		ReplayChunk chunk = ReplayChunk.CreateEmpty();
-		
-		StringBuffer sb = new StringBuffer();
-		
-		private void addChunk() {
-			if(sb.length() == 0) return;
-			String frame = sb.toString();
-			sb.setLength(0);
-			String[] data = SNUtils.split(frame, '|', 4);
-			int delta = Integer.parseInt(data[0]);
-			if (delta == -12345) return;
-			lastTime += delta;
-			int keys = (int)Double.parseDouble(data[1]);
-			if (keys == lastKeys) return;
-			lastKeys = keys;
-			if (nextFrame >= ReplayChunk.FRAMES_IN_CHUNK) {
-				nextFrame = 0;
-				chunk = ReplayChunk.Chain(chunk);
-			}
-			chunk.data[nextFrame * 2] = lastTime;
-			chunk.data[nextFrame * 2 + 1] = keys;
-			chunk.framesCount++;
-			nextFrame++;
+	int lastKeys = 0;
+	int lastTime = 0;
+	int nextFrame = 0;
+	ReplayChunk chunk = ReplayChunk.CreateEmpty();
+
+	StringBuffer sb = new StringBuffer();
+
+	private void addChunk() {
+		if (sb.length() == 0)
+			return;
+		String frame = sb.toString();
+		sb.setLength(0);
+		String[] data = SNUtils.split(frame, '|', 4);
+		int delta = Integer.parseInt(data[0]);
+		if (delta == -12345)
+			return;
+		lastTime += delta;
+		int keys = (int) Double.parseDouble(data[1]);
+		if (keys == lastKeys)
+			return;
+		lastKeys = keys;
+		if (nextFrame >= ReplayChunk.FRAMES_IN_CHUNK) {
+			nextFrame = 0;
+			chunk = ReplayChunk.Chain(chunk);
 		}
-		
-		public void write(int b) throws IOException {
-			if(b == ',') {
-				addChunk();
-				return;
-			}
-			sb.append((char) b);
-		}
-		
-		public void close() {
+		chunk.data[nextFrame * 2] = lastTime;
+		chunk.data[nextFrame * 2 + 1] = keys;
+		chunk.framesCount++;
+		nextFrame++;
+	}
+
+	public void write(int b) throws IOException {
+		if (b == ',') {
 			addChunk();
+			return;
 		}
-		
-		public ReplayChunk getChunk() {
-			return chunk;
-		}
-	};
+		sb.append((char) b);
+	}
+
+	public void close() {
+		addChunk();
+	}
+
+	public ReplayChunk getChunk() {
+		return chunk;
+	}
+};
