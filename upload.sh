ad55b1a7b1e3d0af603d20b2b9712ef6a8b50493
Feodor0090 <53872073+Feodor0090@users.noreply.github.com>
2022-10-30 01:51:43 +0300

    Delete upload.sh

--- upload.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Script to upload CI builds to nnchan
-
-curl -F "debug=@${DBG}" -F "normal=@${NML}" -F "key=${KEY}" -F "branch=${BRANCH}" -F "commit=${COMMIT}" http://nnp.nnchan.ru/nm/dev/ci.php
