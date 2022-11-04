0a2de4fb0b21514d80a6eb400c57aa3f13b1908b
ansel <79257300125@ya.ru>
2022-11-04 23:05:11 +0300

    Add base for checker

---
+++ tests/TestResultsProcessor/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestResultsProcessor;
+
+public static class Program
+{
+    private static int Main(string[] args)
+    {
+        Console.WriteLine("err");
+        return 1;
+    }
+}
